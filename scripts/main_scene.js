b2c6aa645f869626e2e17eb6f31af770a543ccaa
Tyson0723 <t.nagasaki.sys22@morijyobi.ac.jp>
2023-12-20 14:18:41 +0900

    背景の表示背景の表示 close #8,close #9,close #10

---
+++ scripts/main_scene.js
@@ -0,0 +1,23 @@
+// シーンクラス
+// 他のJSファイルから呼び出された場合はシーンを返す
+class MainScene extends Phaser.Scene {
+  // コンストラクタ
+  constructor() {
+    // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
+    super('MainScene');
+  }
+  // シーンの事前読み込み処理
+  preload() {
+    // 画像の読み込み(使用する時の名前, パス)
+    this.load.image('back', 'assets/back.png');
+  }
+  // シーン初期化処理
+  create() {
+    // 単体画像をシーンに追加(X座標,Y座標,画像名)
+    this.add.image(400, 300, 'back');
+  }
+
+  update() {
+
+  }
+}
