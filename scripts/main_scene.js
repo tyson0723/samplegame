e5a703aa874a4b0f7149114600bc7743b363e93d
Tyson0723 <t.nagasaki.sys22@morijyobi.ac.jp>
2023-12-20 14:35:03 +0900

    　太郎と花子をカーソルキーで上下左右に動かす close #12,close #13

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -25,6 +25,54 @@ class MainScene extends Phaser.Scene {
   }
 
   update() {
-
+    // キーボードの情報を取得
+    let cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.up.isDown) {
+      console.log("Up!!");
+      this.taro.setVelocityY(-40);// 上方向の速度を設定
+      this.hanako.setVelocityY(40);// 上方向の速度を設定
+    } else if (cursors.down.isDown) {
+      console.log("down!!");
+      this.taro.setVelocityY(40);// 下方向の速度を設定
+      this.hanako.setVelocityY(-40);// 下方向の速度を設定
+    } else if (cursors.left.isDown) {
+      console.log("Left");
+      this.taro.setVelocityX(-40);// 左方向の速度を設定
+      this.hanako.setVelocityX(40);// 左方向の速度を設定
+    } else if (cursors.right.isDown) {
+      console.log("Right!!");
+      this.taro.setVelocityX(40);// 右方向の速度を設定
+      this.hanako.setVelocityX(-40);// 右方向の速度を設定
+    } else {
+      this.taro.setVelocityX(0);// 横方向の速度を0
+      this.taro.setVelocityY(0);// 縦方向の速度を0
+      this.hanako.setVelocityX(0);// 横方向の速度を0
+      this.hanako.setVelocityY(0);// 縦方向の速度を0
+    }
+  }update() {
+    // キーボードの情報を取得
+    let cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.up.isDown) {
+      console.log("Up!!");
+      this.taro.setVelocityY(-40);// 上方向の速度を設定
+      this.hanako.setVelocityY(40);// 上方向の速度を設定
+    } else if (cursors.down.isDown) {
+      console.log("down!!");
+      this.taro.setVelocityY(40);// 下方向の速度を設定
+      this.hanako.setVelocityY(-40);// 下方向の速度を設定
+    } else if (cursors.left.isDown) {
+      console.log("Left");
+      this.taro.setVelocityX(-40);// 左方向の速度を設定
+      this.hanako.setVelocityX(40);// 左方向の速度を設定
+    } else if (cursors.right.isDown) {
+      console.log("Right!!");
+      this.taro.setVelocityX(40);// 右方向の速度を設定
+      this.hanako.setVelocityX(-40);// 右方向の速度を設定
+    } else {
+      this.taro.setVelocityX(0);// 横方向の速度を0
+      this.taro.setVelocityY(0);// 縦方向の速度を0
+      this.hanako.setVelocityX(0);// 横方向の速度を0
+      this.hanako.setVelocityY(0);// 縦方向の速度を0
+    }
   }
 }
